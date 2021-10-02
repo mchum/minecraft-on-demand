88b36928b22982987d2bbc2d8b12756b79a8a599
Matthew Chum <mchum@securitycompass.com>
2021-10-02 11:22:26 -0400

    working cloudrun

--- infra/README.md
+++ infra/README.md
@@ -3,3 +3,9 @@
 * GCP Cloud Storage for world save
 * Connection detection might be a Cloud Function of some sort
 * No need for Helm or Kustomize since there's no long term permanent EKS stuff
+
+```
+user@localhost:~$ export GOOGLE_APPLICATION_CREDENTIALS=~/.secrets/gcp_creds.json 
+user@localhost:~$ terraform plan -out .tfplan
+user@localhost:~$ terraform apply .tfplan
+```
--- infra/main.tf
+++ infra/main.tf
@@ -1,30 +1,44 @@
-provider "aws" {
-  project = "minecraft-on-demand"
-  region = "us-east1"
+provider "google" {
+  project       = "minecraft-on-demand-327813"
+  region        = "us-east1"
 }
 
 resource "google_cloud_run_service" "minecraft" {
     count       = var.enabled ? 1 : 0
     name        = "minecraft-server"
     location    = "us-east1"
+
     template {
-        metadata = {
-            name    = "minecraft-server"
-            annotations {
+        metadata {
+            name    = "minecraft-server-fabric"
+            annotations = {
                 "autoscaling.knative.dev/maxScale"  = "1"
             }
         }
+
         spec {
             containers {
-                image           = "itzg/minecraft-server"
-                port            = 25565
+                image           = "us-east1-docker.pkg.dev/minecraft-on-demand-327813/minecraft-server/minecraft-server:latest"
+                ports {
+                    container_port  = 25565
+                }
                 env {
                     name    = "TYPE"
                     value   = "FABRIC"
                 }
+                env {
+                    name    = "EULA"
+                    value   = "TRUE"
+                }
+                resources {
+                    limits = {
+                        "memory"  = "4Gi"
+                    }
+                }
             }
         }
     }
+
     traffic {
         percent         = 100
         latest_revision = true
